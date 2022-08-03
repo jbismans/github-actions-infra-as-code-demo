831c3d9efe4df8e83b10f42dcfb1afe72d6e7549
jbismans <jbismans@fortinet.com>
2022-08-03 11:47:56 +0200

    Automation code update

--- Azure/02-fortigate.tf
+++ Azure/02-fortigate.tf
@@ -126,6 +126,7 @@ data "template_file" "fgt_custom_data" {
     fgt_internal_gw     = var.gateway_ipaddress["2"]
     vnet_network        = var.vnet
     prefix              = var.PREFIX
+    az_token_webhook    = var.AZ_TOKEN_WEBHOOK
   }
 }
 
